9432f37f41af6d020d379b9b5cbc82f2facb2e8c
zhekkkk <fedo5eeW@yandex.ru>
2024-04-03 12:40:18 +0300

    feat: add default objects

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,14 +11,17 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) {
-        DepartmentServiceImpl.getInstance().add(new Department("abracadabra"));
-        DepartmentServiceImpl.getInstance().add(new Department("woopytyscoop"));
-        DepartmentServiceImpl.getInstance().add(new Department("scoobydoobydoo"));
+        DepartmentServiceImpl.getInstance().add(new Department("accounting"));
+        DepartmentServiceImpl.getInstance().add(new Department("sales"));
+        DepartmentServiceImpl.getInstance().add(new Department("human resources"));
         List<Department> deps = DepartmentServiceImpl.getInstance().getAll();
-        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(0).getId(), "kanye", "west", "shiz", 34, 15000));
-        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(0).getId(), "marshall", "mathers", "shiz", 55, 22222));
-        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(0).getId(), "snoop", "dogg", "shiz", 12, 150));
-        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(0).getId(), "colson", "baker", "shiz", 99, 87000));
+        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(0).getId(), "kuznetsov", "roman", "egorovich", 34, 15000));
+        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(0).getId(), "pugacheva", "kira", "georgievna", 55, 22222));
+        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(0).getId(), "kovaleva", "milana", "kirillovna", 25, 1509));
+        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(1).getId(), "samoylova", "kseniya", "maksimovna", 52, 87000));
+        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(1).getId(), "maslova", "arina", "andreevna", 22, 8700));
+        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(2).getId(), "demin", "mikhail", "maratovich", 44, 28974));
+        EmployeeServiceImpl.getInstance().add(new Employee(deps.get(2).getId(), "larionov", "artem", "fyodorovich", 33, 56453));
         Menu.run();
     }
 
